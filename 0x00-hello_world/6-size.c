3a08eefce1da5f39e57999b88c15ec85bfe53ccf
Emiraell <emmzex19@gmail.com>
2022-12-08 18:14:13 -0800

    I checked sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ float floatType;
 printf("Size of a char: %ld byte(s)\n", sizeof(charType));
 printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
 prinft("Size of a long int: %ld byte(s)\n", sizeof(longintegerType));
-printf("Size of a long long: %ld byte(s)\n", sizeof(longlongintegerType));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintegerType));
 printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 return (0);
 }
