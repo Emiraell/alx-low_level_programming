f0361b7f3322835310eab216e3b2ade3df2b459a
Emiraell <emmzex19@gmail.com>
2022-12-14 10:12:30 -0800

    I made some changes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 
 int m;
 char i;
-for (m=1, m<=10, m++)
+for (m=1; m<=10; m++)
 {
-for (i='a', i<='z', i++)
+for (i='a'; i<='z'; i++)
 _putchar(i);
 }
 _putchar('\n');
