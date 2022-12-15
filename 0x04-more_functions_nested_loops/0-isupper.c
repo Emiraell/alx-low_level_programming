71f75633f80941c805fb035121c4fcb735412cef
Emiraell <emmzex19@gmail.com>
2022-12-15 06:50:22 -0800

    I added some files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - To check if character is uppercase
+ * @c: the character to check
+ * Return: 1 if c is uppercase, otherwise 0
+ */
+int _isupper(int c)
+{
+	int c;
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		_putchar("%d\n", c)
+	}
+	return (c);
+}
