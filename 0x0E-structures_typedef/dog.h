5ba51775ebda9a74e3e5bcc9e25ae7b6f4094dda
Emiraell <emmzex19@gmail.com>
2023-01-17 16:12:47 -0800

    I mafde some changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,6 +6,7 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ *
  */
 
 struct dog
@@ -18,7 +19,6 @@ struct dog
 /**
  * dog_t -Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
