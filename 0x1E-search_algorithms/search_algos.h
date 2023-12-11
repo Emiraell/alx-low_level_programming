5649a2106a569d8e989942a726e8c54692a796da
root <emmzex19@gmail.com>
2023-12-11 06:00:30 -0800

    Search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,19 +1,19 @@
-#ifndef _SEARCH_ALGORITHM_H_
-#define _SEARCH_ALGORITHM_H_
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
 
 #include <stdlib.h>
 #include <stdio.h>
 
 /**
- * struct listint_s -singly linked list
+ * struct listint_s - singly linked list
  *
- * @n: integer
- * @index: index of the node in the list
- * @next: pointer to the next node
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
-
 typedef struct listint_s
 {
 	int n;
@@ -23,13 +23,15 @@ typedef struct listint_s
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
+ *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
+ *
  * Description: singly linked list node structure with an express lane
+ * for Holberton project
  */
-
 typedef struct skiplist_s
 {
 	int n;
